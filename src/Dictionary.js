adc45deee7695ae14b82ddf6d5dd438781aeff12
melika760 <72795928+melika760@users.noreply.github.com>
2022-09-05 09:40:08 +0430

    add axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,22 @@
 import React,{useState}from 'react';
+import axios from 'axios';
 export default function Dictionary(){
 const[keyword,Setkeyword]= useState('')
 function handlesubmit(event){
 event.preventDefault();
-alert(`Searchinhg for ${keyword}`)
+let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+axios.get(apiUrl).then(handleResponse)
+}
+function handleResponse(response){
+    console.log(response.data)
 }
 function Changkeyword(event){
     Setkeyword(event.target.value)
 }
     return(
         <div className='dictionary container'>
-            <h1 className='text-center'>Dictionary</h1>
-            <form className='text-center'onSubmit={handlesubmit}>
+            <h1 >Dictionary</h1>
+            <form onSubmit={handlesubmit}>
                 <input type="search" autoFocus={true} onChange={Changkeyword}/>
                 <input type="submit" className='btn btn-primary'/>
             </form>
