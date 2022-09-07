a97cf0d79a69f404e0cc30449b2ac08cc233c415
melika760 <72795928+melika760@users.noreply.github.com>
2022-09-07 09:21:48 +0430

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
 export default function Results(props){
-    console.log(props.results)
     if(props.results){return(
         <div className="Results">
         <h4>{props.results.word}</h4>
